d5a43c95bfb397dc8049d120202eb241570f021f
Michael <m.m.downing@bigpond.com>
2024-01-31 20:41:03 +0800

    updated navigation screens

--- drink-tank/App.tsx
+++ drink-tank/App.tsx
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./index";
-import About from "./about";
+import HomeScreen from "./pages/index";
+import About from "./pages/about";
+import AddPlayers from "./pages/addplayers";
+import Games from "./pages/games";
+import MainGame from "./pages/maingame";
+import Settings from "./pages/settings";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -12,6 +16,10 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="AddPlayers" component={AddPlayers} />
+        <Stack.Screen name="Games" component={Games} />
+        <Stack.Screen name="MainGame" component={MainGame} />
+        <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
