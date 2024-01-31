2e783b054b3360a4745deaa8e23b150a72dfc5de
Michael <m.m.downing@bigpond.com>
2024-01-31 20:38:20 +0800

    created starting folder stucture

--- drink-tank/pages/games.tsx
+++ drink-tank/pages/games.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Games() {
   return (
     <View style={styles.container}>
-      <Text>This is the About page</Text>
+      <Text>This is the Games page</Text>
       <StatusBar style="auto" />
     </View>
   );
